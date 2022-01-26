d4ec8f3d7ee2ec4d115b3751530a00b08e86547a
vehikl <go@vehikl.com>
2022-01-26 17:05:56 -0500

    WIP - failing on incorrect tests

--- src/components/Trivia.vue
+++ src/components/Trivia.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <div aria-label="theQuestion">{{ question }}</div>
+    <div aria-label="the question">{{ question }}</div>
     <button @click="getNewQuestion">Click Me for a new question!</button>
+    <div aria-label="correct answer">{{ correctAnswer }}</div>
+    <div aria-label="incorrect answers">
+      <div aria-label="incorrect answer">{{ wrongAnswers[0] }}</div>
+      <div aria-label="incorrect answer">{{ wrongAnswers[1] }}</div>
+      <div aria-label="incorrect answer">{{ wrongAnswers[2] }}</div>
+    </div>
   </div>
 </template>
 
@@ -9,22 +15,24 @@
 import axios from "axios";
 import {ref} from 'vue'
 
-
-
 export default {
   name: "Trivia",
   setup(){
     let result = "hello";
     const question = ref(null);
+    const correctAnswer = ref(null);
+    const wrongAnswers = ref([]);
 
     async function getNewQuestion() {
       await axios.get('https://opentdb.com/api.php?amount=1')
           .then(response => {
             question.value = response.data.results?.[0].question;
+            correctAnswer.value = response.data.results?.[0].correct_answer;
+            wrongAnswers.value = response.data.results?.[0].incorrect_answers;
           })
     }
 
-    return { result, question, getNewQuestion }
+    return { result, question, getNewQuestion, correctAnswer, wrongAnswers }
   }
 
 
--- tests/unit/trivia.spec.js
+++ tests/unit/trivia.spec.js
@@ -1,5 +1,6 @@
 import {shallowMount} from '@vue/test-utils'
 import Trivia from '@/components/Trivia.vue'
+import {nextTick} from "vue";
 
 var axios = require("axios")
 var MockAdapter = require('axios-mock-adapter')
@@ -7,25 +8,59 @@ var mock = new MockAdapter(axios);
 
 
 describe('TriviaTest.vue', () => {
-    const expectedResponse = {
-        "category": "Science: Computers",
-        "type": "multiple",
-        "difficulty": "easy",
-        "question": "How long is an IPv6 address?",
-        "correct_answer": "128 bits",
-        "incorrect_answers": ["32 bits", "64 bits", "128 bytes"]
+    const sampleResponse = {
+        "response_code": 0,
+        "results": [
+            {
+                "category": "Entertainment: Television",
+                "type": "multiple",
+                "difficulty": "medium",
+                "question": "The fictional movie &#039;Rochelle, Rochelle&#039; features in which sitcom?",
+                "correct_answer": "Seinfeld",
+                "incorrect_answers": [
+                    "Frasier",
+                    "Cheers",
+                    "Friends"
+                ]
+            }
+        ]
     };
 
     it('can consume the trivia api', async () => {
+        mock.onGet('https://opentdb.com/api.php?amount=1').reply(200, sampleResponse)
+        const expectedResponse = sampleResponse.results?.[0].question;
 
-        mock.onGet('https://opentdb.com/api.php?amount=1').reply(200, expectedResponse)
         const triviaWrapper = shallowMount(Trivia);
         await triviaWrapper.find('button').trigger('click');
-        const triviaQuestion = triviaWrapper.find("[aria-label='theQuestion']")
-console.log(triviaQuestion);
-        expect(triviaQuestion.text()).toContain(expectedResponse.question);
-        //expect(axios.get).toHaveBeenCalledTimes(1);
-        //expect(axios.get).toHaveBeenLastCalledWith('https://opentdb.com/api.php?amount=1')
+        const triviaQuestion = triviaWrapper.find("[aria-label='the question']")
 
+        expect(triviaQuestion.text()).toContain(expectedResponse)
+    });
+
+    it('displays the correct answer', async () => {
+        mock.onGet('https://opentdb.com/api.php?amount=1').reply(200, sampleResponse)
+        const expectedResponse = sampleResponse.results[0].correct_answer;
+
+        const triviaWrapper = shallowMount(Trivia);
+        await triviaWrapper.find('button').trigger('click');
+        const correctAnswer = triviaWrapper.find("[aria-label='correct answer']")
+
+        expect(correctAnswer.text()).toContain(expectedResponse)
+    })
+
+    it('displays the incorrect answer', async () => {
+        // arrange
+        mock.onGet('https://opentbd.com/api.php?amount=1').reply(200, sampleResponse)
+        const expectedResponse = sampleResponse.results[0].incorrect_answers;
+
+        const triviaWrapper = shallowMount(Trivia);
+        await triviaWrapper.find('button').trigger('click');
+        const wrongAnswer = triviaWrapper.findAll("[aria-label='incorrect answer']");
+        // await nextTick();
+        // expect(wrongAnswer.text()).toContain(expectedResponse);
+
+        expectedResponse.forEach( function (incorrectAnswer) {
+            expect(incorrectAnswer).toContain(wrongAnswer.text())
+        })
     })
 })
