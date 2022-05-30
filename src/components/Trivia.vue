dc57b88bbeda2ded8659c7a5bf66cc1c078c52a4
Ian <i.ewing@vehikl.com>
2022-05-30 17:01:13 -0400

    WIP - Getting tests green/Refactor to use div instead of window alert

--- src/components/Trivia.vue
+++ src/components/Trivia.vue
@@ -1,21 +1,43 @@
 <template>
   <div>
     <div aria-label="the question">{{ question }}</div>
-    <button @click="getNewQuestion">Click Me for a new question!</button>
-    <div aria-label="correct answer">
-      <input type="radio" name="answer" id="answer1">{{ correctAnswer }}
+    <button
+      aria-label="Click Me for a new question!"
+      @click="getNewQuestion"
+      class="text-red-600"
+    >
+      Click Me for a new question!
+    </button>
+    <div
+      aria-label="Possible Answers"
+      v-for="(choice, index) in choiceAnswers"
+      :key="index"
+    >
+      <input
+        type="radio"
+        name="answer"
+        id="answer{{index}}"
+        :value="choice"
+        v-model="selection"
+      />{{ choice }}
     </div>
-    <div aria-label="incorrect answers">
-      <div aria-label="incorrect answer"><input type="radio" name="answer" id="answer2">{{ wrongAnswers[0] }}</div>
-      <div aria-label="incorrect answer"><input type="radio" name="answer" id="answer3">{{ wrongAnswers[1] }}</div>
-      <div aria-label="incorrect answer"><input type="radio" name="answer" id="answer4">{{ wrongAnswers[2] }}</div>
+    <div aria-label="Answer Results">
+      {{ resultMessage }}
     </div>
+    <button
+      v-if="choiceAnswers.length > 0"
+      class="bg-blue-500 font-bold text-white rounded-sm px-3 py-1"
+      aria-label="Submit"
+      @click="validateAnswer"
+    >
+      Submit
+    </button>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import {ref} from 'vue'
+import { ref } from "vue";
 
 export default {
   name: "Trivia",
@@ -23,25 +45,64 @@ export default {
     let result = "hello";
     const question = ref(null);
     const correctAnswer = ref(null);
-    const wrongAnswers = ref([]);
+    const choiceAnswers = ref([]);
+    const isCorrectAnswer = ref(false);
+    const selection = ref(null);
+    const resultMessage = ref(null);
 
     async function getNewQuestion() {
-      await axios.get('https://opentdb.com/api.php?amount=1')
-          .then(response => {
-            question.value = response.data.results?.[0].question;
-            correctAnswer.value = response.data.results?.[0].correct_answer;
-            wrongAnswers.value = response.data.results?.[0].incorrect_answers;
-          })
+      await axios
+        .get("https://opentdb.com/api.php?amount=1")
+        .then((response) => {
+          question.value = response.data.results?.[0].question;
+          correctAnswer.value = response.data.results?.[0].correct_answer;
+          choiceAnswers.value = response.data.results?.[0].incorrect_answers;
+          choiceAnswers.value.push(correctAnswer.value);
+        });
+      choiceAnswers.value = shuffle(choiceAnswers.value);
     }
 
-    return {result, question, getNewQuestion, correctAnswer, wrongAnswers}
-  }
+    function validateAnswer() {
+      if (selection.value === correctAnswer.value) {
+        resultMessage.value = "Correct!";
+      }
+      resultMessage.value = "NO!";
+      getNewQuestion();
+    }
 
+    function shuffle(array) {
+      let currentIndex = array.length,
+        randomIndex;
 
-}
+      // While there remain elements to shuffle...
+      while (currentIndex !== 0) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
 
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex],
+          array[currentIndex],
+        ];
+      }
+
+      return array;
+    }
+
+    return {
+      result,
+      question,
+      getNewQuestion,
+      correctAnswer,
+      choiceAnswers,
+      validateAnswer,
+      isCorrectAnswer,
+      selection,
+      resultMessage,
+    };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- tests/unit/trivia.spec.js
+++ tests/unit/trivia.spec.js
@@ -1,66 +1,153 @@
-import {shallowMount} from '@vue/test-utils'
-import Trivia from '@/components/Trivia.vue'
+import { shallowMount } from "@vue/test-utils";
+import Trivia from "@/components/Trivia.vue";
 
-var axios = require("axios")
-var MockAdapter = require('axios-mock-adapter')
-var mock = new MockAdapter(axios);
+const axios = require("axios");
+const MockAdapter = require("axios-mock-adapter");
+const mock = new MockAdapter(axios);
 
-const BASE_URL = 'https://opentdb.com/api.php?amount=1';
+const BASE_URL = "https://opentdb.com/api.php?amount=1";
 const sampleResponse = {
-    "response_code": 0,
-    "results": [
-        {
-            "category": "Entertainment: Television",
-            "type": "multiple",
-            "difficulty": "medium",
-            "question": "The fictional movie &#039;Rochelle, Rochelle&#039; features in which sitcom?",
-            "correct_answer": "Seinfeld",
-            "incorrect_answers": [
-                "Frasier",
-                "Cheers",
-                "Friends"
-            ]
-        }
-    ]
+  response_code: 0,
+  results: [
+    {
+      category: "Entertainment: Television",
+      type: "multiple",
+      difficulty: "medium",
+      question:
+        "The fictional movie &#039;Rochelle, Rochelle&#039; features in which sitcom?",
+      correct_answer: "Seinfeld",
+      incorrect_answers: ["Frasier", "Cheers", "Friends"],
+    },
+  ],
 };
-mock.onGet(BASE_URL).reply(200, sampleResponse)
+// const sampleResponseTwo = {
+//     "response_code": 0,
+//     "results": [{
+//         "category": "Entertainment: Music",
+//         "type": "boolean",
+//         "difficulty": "medium",
+//         "question": "For his performance at ComplexCon 2016 in Long Beach, California, Skrillex revived his &quot;Mothership&quot; set piece for one night only.",
+//         "correct_answer": "True",
+//         "incorrect_answers": ["False"]
+//     }]
+// };
+// mock.onGet(BASE_URL).replyOnce(200, sampleResponse).onGet(BASE_URL).replyOnce(200, sampleResponseTwo);
 
-describe('TriviaTest.vue', () => {
+describe("TriviaTest.vue", () => {
+  beforeEach(() => {
+    window.alert = jest.fn();
+    mock.onGet(BASE_URL).reply(200, sampleResponse);
+  });
 
-    it('can consume the trivia api', async () => {
-        const expectedResponse = sampleResponse.results?.[0].question;
+  afterEach(() => {
+    mock.reset();
+  });
 
-        const triviaWrapper = shallowMount(Trivia);
-        await triviaWrapper.find('button').trigger('click');
-        const triviaQuestion = triviaWrapper.find("[aria-label='the question']")
+  it("can consume the trivia api", async () => {
+    const expectedResponse = sampleResponse.results?.[0].question;
 
-        expect(triviaQuestion.text()).toContain(expectedResponse)
+    const triviaWrapper = shallowMount(Trivia);
+    await triviaWrapper.find("button").trigger("click");
+    const triviaQuestion = triviaWrapper.find("[aria-label='the question']");
+
+    expect(triviaQuestion.text()).toContain(expectedResponse);
+  });
+
+  it("displays the possible answers", async () => {
+    const expectedResponse = sampleResponse.results[0].correct_answer;
+    const expectedAnswers = sampleResponse.results[0].incorrect_answers;
+    expectedAnswers.push(expectedResponse);
+
+    const triviaWrapper = shallowMount(Trivia);
+    await triviaWrapper.find("button").trigger("click");
+    const possibleAnswers = triviaWrapper.findAll(
+      "[aria-label='Possible Answers']"
+    );
+    let arrayOfAnswers = [];
+    for (let i = 0; i < possibleAnswers.length; ++i) {
+      arrayOfAnswers.push(possibleAnswers[i].text());
+    }
+
+    expectedAnswers.forEach(function (expected) {
+      expect(arrayOfAnswers).toContain(expected);
     });
+  });
 
-    it('displays the correct answer', async () => {
-        const expectedResponse = sampleResponse.results[0].correct_answer;
+  it("renders a submit button with appropriate text", async () => {
+    const triviaWrapper = shallowMount(Trivia);
 
-        const triviaWrapper = shallowMount(Trivia);
-        await triviaWrapper.find('button').trigger('click');
-        const correctAnswer = triviaWrapper.find("[aria-label='correct answer']")
+    await triviaWrapper
+      .find("[aria-label='Click Me for a new question!']")
+      .trigger("click");
 
-        expect(correctAnswer.text()).toContain(expectedResponse)
-    })
+    const submitButton = triviaWrapper.find("[aria-label='Submit']");
+    expect(submitButton.isVisible()).toBe(true);
+    expect(submitButton.text()).toBe("Submit");
+  });
 
-    it('displays the incorrect answer', async () => {
-        const expectedWrongAnswers = sampleResponse.results[0].incorrect_answers;
+  it("can submit answer to question", async () => {
+    const triviaWrapper = shallowMount(Trivia);
 
-        const triviaWrapper = shallowMount(Trivia);
-        await triviaWrapper.find('button').trigger('click');
-        const wrongAnswers = triviaWrapper.findAll("[aria-label='incorrect answer']");
+    await triviaWrapper.find("button").trigger("click");
+    const answer = triviaWrapper.find("[aria-label='Possible Answers']");
+    await answer.trigger("click");
 
-        let arrayOfAnswers = [];
-        for (let i = 0; i < wrongAnswers.length; ++i) {
-            arrayOfAnswers.push(wrongAnswers[i].text());
-        }
+    const submit = triviaWrapper.find("[aria-label='Submit']");
+    await submit.trigger("click");
 
-        expectedWrongAnswers.forEach( function (incorrectAnswer) {
-            expect(arrayOfAnswers).toContain(incorrectAnswer)
-        })
-    })
-})
+    expect(
+      triviaWrapper.find("[aria-label='Answer Results']").isVisible()
+    ).toBe(true);
+  });
+
+  it("can tell the user if they chose the correct answer", async () => {
+    const triviaWrapper = shallowMount(Trivia);
+    const correctAnswer = sampleResponse.results[0].correct_answer;
+
+    await triviaWrapper
+      .find("[aria-label='Click Me for a new question!']")
+      .trigger("click");
+    await triviaWrapper.find("input", { text: correctAnswer }).trigger("click");
+    triviaWrapper.find("[aria-label='Submit']").trigger("click");
+
+    const answerResult = await triviaWrapper.find(
+      '[aria-label="Answer Results"]'
+    );
+    expect(answerResult.text()).toContain("Correct!");
+  });
+
+  it("can tell the user if they chose the wrong answer", async () => {
+    const triviaWrapper = shallowMount(Trivia);
+    const incorrectAnswer = sampleResponse.results[0].incorrect_answers[0];
+
+    await triviaWrapper
+      .find("[aria-label='Click Me for a new question!']")
+      .trigger("click");
+    await triviaWrapper
+      .find("input", { text: incorrectAnswer })
+      .trigger("click");
+    triviaWrapper.find("[aria-label='Submit']").trigger("click");
+
+    const result = await triviaWrapper.find('[aria-label="Answer Results"]');
+    expect(result.text()).toContain("NO!");
+  });
+
+  it("loads a new question after answering the question", async () => {
+    window.alert = jest.fn();
+
+    const triviaWrapper = shallowMount(Trivia);
+    const incorrectAnswer = sampleResponse.results[0].incorrect_answers[0];
+
+    await triviaWrapper
+      .find("[aria-label='Click Me for a new question!']")
+      .trigger("click");
+    await triviaWrapper
+      .find("input", { text: incorrectAnswer })
+      .trigger("click");
+    await triviaWrapper.find("[aria-label='Submit']").trigger("click");
+
+    // find the question text for the second question
+    const question = await triviaWrapper.find('[aria-label="the question"]');
+    expect(question.text()).toContain(sampleResponse.results[0].question);
+  });
+});
